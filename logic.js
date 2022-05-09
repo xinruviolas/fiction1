8c7426683c17aa2d22f0080c9831d498701a8ab4
Xinru Li <xinru_li@brown.edu>
2022-05-08 21:07:50 -0400

    logic

--- logic.js
+++ logic.js
@@ -1,18 +1,112 @@
-function about() {
-    document.getElementById("about").style.display = "block";
-    document.getElementById("artiststatement").style.display = "none";
+function home() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("TheStoryCollector").style.display = "none";
+    document.getElementById("TheMushroomHouse").style.display = "none";
+    document.getElementById("Florence").style.display = "none";
+    document.getElementById("Halpie").style.display = "none";
+    document.getElementById("Shiny").style.display = "none";
+    document.getElementById("Gizem").style.display = "none";
 
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace("active", "");
-    document.getElementById("about1").className += " active";
+    document.getElementById("homeid").className += " active";
 
 }
 
-function artiststatement() {
-    document.getElementById("about").style.display = "none";
-    document.getElementById("artiststatement").style.display = "block";
+function TheStoryCollector() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("TheStoryCollector").style.display = "block";
+    document.getElementById("TheMushroomHouse").style.display = "none";
+    document.getElementById("Florence").style.display = "none";
+    document.getElementById("Halpie").style.display = "none";
+    document.getElementById("Shiny").style.display = "none";
+    document.getElementById("Gizem").style.display = "none";
 
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace("active", "");
-    document.getElementById("artiststatement1").className += " active";
+    document.getElementById("TheStoryCollectorid").className += " active";
 }
+
+function TheMushroomHouse() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("TheStoryCollector").style.display = "none";
+    document.getElementById("TheMushroomHouse").style.display = "block";
+    document.getElementById("Florence").style.display = "none";
+    document.getElementById("Halpie").style.display = "none";
+    document.getElementById("Shiny").style.display = "none";
+    document.getElementById("Gizem").style.display = "none";
+
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    document.getElementById("TheMushroomHouseid").className += " active";
+}
+
+function Florence() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("TheStoryCollector").style.display = "none";
+    document.getElementById("TheMushroomHouse").style.display = "none";
+    document.getElementById("Florence").style.display = "block";
+    document.getElementById("Halpie").style.display = "none";
+    document.getElementById("Shiny").style.display = "none";
+    document.getElementById("Gizem").style.display = "none";
+
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    document.getElementById("Florenceid").className += " active";
+}
+
+function Halpie() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("TheStoryCollector").style.display = "none";
+    document.getElementById("TheMushroomHouse").style.display = "none";
+    document.getElementById("Florence").style.display = "none";
+    document.getElementById("Halpie").style.display = "block";
+    document.getElementById("Shiny").style.display = "none";
+    document.getElementById("Gizem").style.display = "none";
+
+
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    document.getElementById("Halpieid").className += "active";
+}
+
+
+function Shiny() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("TheStoryCollector").style.display = "none";
+    document.getElementById("TheMushroomHouse").style.display = "none";
+    document.getElementById("Florence").style.display = "none";
+    document.getElementById("Halpie").style.display = "none";
+    document.getElementById("Shiny").style.display = "block";
+    document.getElementById("Gizem").style.display = "none";
+
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    document.getElementById("Shinyid").className += " active";
+}
+
+function Gizem() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("TheStoryCollector").style.display = "none";
+    document.getElementById("TheMushroomHouse").style.display = "none";
+    document.getElementById("Florence").style.display = "none";
+    document.getElementById("Halpie").style.display = "none";
+    document.getElementById("Shiny").style.display = "none";
+    document.getElementById("Gizem").style.display = "block";
+
+
+
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    document.getElementById("Gizemid").className += " active";
+}
+
+
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
